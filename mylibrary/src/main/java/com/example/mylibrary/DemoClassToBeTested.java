a6775d26c9b81bb92b46b0ab8c79c4eb7b7701c8
abhijit.296 <das.abhijit.abhijit65@gmail.com>
2019-03-09 17:55:49 +0530

    changed on 10th march

--- mylibrary/src/main/java/com/example/mylibrary/DemoClassToBeTested.java
+++ mylibrary/src/main/java/com/example/mylibrary/DemoClassToBeTested.java
@@ -1,4 +1,17 @@
 package com.example.mylibrary;
 
+import android.content.Context;
+
 public class DemoClassToBeTested {
+
+//    public int addTwoValues(Context ctx)
+   public String addTwoValues(Context ctx)
+    {
+      //  ctx.get
+//        ctx.getString(R.integer.app_value)
+//        return ctx.getString(R.integer.app_value)
+//        return ctx.getResources().getInteger(R.integer.app_value);
+
+        return ctx.getString(R.string.app_name);
+    }
 }
